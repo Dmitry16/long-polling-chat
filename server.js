73648b384dc01793e0b0bfee5bc9eb82015d1253
Dmitry16 <europartner@inbox.ru>
2019-07-16 20:55:17 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+let http = require('http');
+let fs = require('fs');
+
+http.createServer(function(req, res) {
+
+  switch (req.url) {
+
+    case '/':
+      sendFile('index.html', res);
+      break;
+
+    case '/subscribe':
+      chat.subscribe(req, res);
+      break;
+      
+      case '/publish':
+      chat.publish();
+      break;
+
+    default:
+      res.statusCode = 404;
+      res.end('Not found');
+  }
+
+}).listen(3333);
+
+function sendFile(fileName, res) {
+
+  let fileStream = fs.createReadStream(fileName);
+  fileStream
+    .on('error', function() {
+      res.statusCode = 500;
+      res.end('Server error');
+
+    })
+}
